bc461c39a1c3910205f0161dcfe8f0e71bf8a0bf
Zahraa Qawariq <zahradeeb98@gmail.com>
2025-12-07 15:22:55 +0200

    feat: add validation to the form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,7 +3,23 @@ import { FiUser, FiMail } from "react-icons/fi";
 import { LOGIN_TEXT } from "@/constants/texts";
 import { Link } from "react-router-dom";
 
-export default function LoginForm() {
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/validation/loginSchema";
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log("Login Data:", data);
+  };
+
   return (
     <Card className="w-full max-w-lg p-10 shadow-2xl rounded-3xl bg-white z-10">
       <CardContent className="space-y-7">
@@ -14,51 +30,64 @@ export default function LoginForm() {
           <p className="text-gray-500 mt-2">{LOGIN_TEXT.subtitle}</p>
         </div>
 
-        <div className="space-y-2">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-7">
           <div className="relative">
             <FiUser className="absolute left-3 top-3 text-gray-500" />
             <Input
               type="text"
               placeholder="Enter your name"
-              className="pl-10 py-5 border-[#349FBD] focus-visible:ring-[#349FBD]"
+              {...register("name")}
+              className="pl-10 py-5 border-[#349FBD]"
             />
+            {errors.name && (
+              <p className="text-red-600 text-sm mt-1">{errors.name.message}</p>
+            )}
           </div>
-        </div>
-        <div>
+
           <div className="relative">
             <FiMail className="absolute left-3 top-3 text-gray-500" />
             <Input
               type="email"
               placeholder="Enter your email"
-              className="pl-10 py-5 border-[#349FBD] focus-visible:ring-[#349FBD]"
+              {...register("email")}
+              className="pl-10 py-5 border-[#349FBD]"
             />
+            {errors.email && (
+              <p className="text-red-600 text-sm mt-1">
+                {errors.email.message}
+              </p>
+            )}
           </div>
-        </div>
+          <div className="flex items-center justify-between text-sm">
+            <label className="flex items-center gap-2">
+              <input type="checkbox" className="h-4 w-4 text-[#349FBD]" />
+              <span className="text-gray-500">{LOGIN_TEXT.remember}</span>
+            </label>
 
-        <div className="flex items-center justify-between text-sm">
-          <label className="flex items-center gap-2">
-            <input type="checkbox" className="h-4 w-4 text-[#349FBD]" />
-            <span className="text-gray-500 ">{LOGIN_TEXT.remember}</span>
-          </label>
-          <button className="text-[#2c4e9d] hover:underline font-semibold">
-            {LOGIN_TEXT.forgotPassword}
-          </button>
-        </div>
-
-        <Button className="w-full text-md bg-[#2c4e9d] hover:bg-[#1d3470] text-white py-6 rounded-md">
-          {LOGIN_TEXT.loginButton}
-        </Button>
-
-        <p className="text-gray-600">
-          {LOGIN_TEXT.noAccount}{" "}
-          <Link
-            to="/signup"
-            className="text-[#2c4e9d] font-semibold hover:underline cursor-pointer"
+            <button className="text-[#2c4e9d] hover:underline font-semibold">
+              {LOGIN_TEXT.forgotPassword}
+            </button>
+          </div>
+          <Button
+            type="submit"
+            className="w-full text-md bg-[#2c4e9d] hover:bg-[#1d3470] text-white py-6 rounded-md"
           >
-            {LOGIN_TEXT.signup}
-          </Link>
-        </p>
+            {LOGIN_TEXT.loginButton}
+          </Button>
+
+          <p className="text-gray-600">
+            {LOGIN_TEXT.noAccount}{" "}
+            <Link
+              to="/signup"
+              className="text-[#2c4e9d] font-semibold hover:underline"
+            >
+              {LOGIN_TEXT.signup}
+            </Link>
+          </p>
+        </form>
       </CardContent>
     </Card>
   );
-}
+};
+
+export default LoginForm;
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -3,7 +3,23 @@ import { Link } from "react-router-dom";
 import { FiUser, FiMail, FiLock } from "react-icons/fi";
 import { SIGHN_UP_TEXT } from "@/constants/texts";
 
-export default function SignupForm() {
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signupSchema } from "@/validation/signupSchema";
+
+const SignupForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log("Signup Data:", data);
+  };
+
   return (
     <Card className="w-full max-w-lg p-4 shadow-2xl rounded-3xl bg-white z-10">
       <CardContent className="space-y-7">
@@ -14,61 +30,78 @@ export default function SignupForm() {
           <p className="text-gray-500 mt-2">{SIGHN_UP_TEXT.subtitle}</p>
         </div>
 
-        <div className="space-y-7">
-          <div>
-            <div className="relative">
-              <FiUser className="absolute left-3 top-3 text-gray-500" />
-              <Input
-                type="text"
-                placeholder="Enter your full name"
-                className="pl-10 py-5 border-[#349FBD] focus-visible:ring-[#349FBD]"
-              />
-            </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-7">
+          <div className="relative">
+            <FiUser className="absolute left-3 top-3 text-gray-500" />
+            <Input
+              type="text"
+              placeholder="Enter your full name"
+              {...register("fullName")}
+              className="pl-10 py-5 border-[#349FBD]"
+            />
+            {errors.fullName && (
+              <p className="text-red-600 text-sm mt-1">
+                {errors.fullName.message}
+              </p>
+            )}
           </div>
-          <div>
-            <div className="relative">
-              <FiMail className="absolute left-3 top-3 text-gray-500" />
-              <Input
-                type="email"
-                placeholder="Enter your email"
-                className="pl-10 py-5 border-[#349FBD] focus-visible:ring-[#349FBD]"
-              />
-            </div>
+          <div className="relative">
+            <FiMail className="absolute left-3 top-3 text-gray-500" />
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              {...register("email")}
+              className="pl-10 py-5 border-[#349FBD]"
+            />
+            {errors.email && (
+              <p className="text-red-600 text-sm mt-1">
+                {errors.email.message}
+              </p>
+            )}
           </div>
-          <div>
-            <div className="relative">
-              <FiLock className="absolute left-3 top-3 text-gray-500" />
-              <Input
-                type="password"
-                placeholder="Enter your password"
-                className="pl-10 py-5 border-[#349FBD] focus-visible:ring-[#349FBD]"
-              />
-            </div>
+          <div className="relative">
+            <FiLock className="absolute left-3 top-3 text-gray-500" />
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              {...register("password")}
+              className="pl-10 py-5 border-[#349FBD]"
+            />
+            {errors.password && (
+              <p className="text-red-600 text-sm mt-1">
+                {errors.password.message}
+              </p>
+            )}
           </div>
-          <div>
-            <div className="relative">
-              <FiLock className="absolute left-3 top-3 text-gray-500" />
-              <Input
-                type="password"
-                placeholder="Confirm password"
-                className="pl-10 py-5 border-[#349FBD] focus-visible:ring-[#349FBD]"
-              />
-            </div>
+          <div className="relative">
+            <FiLock className="absolute left-3 top-3 text-gray-500" />
+            <Input
+              type="password"
+              placeholder="Confirm password"
+              {...register("confirmPassword")}
+              className="pl-10 py-5 border-[#349FBD]"
+            />
+            {errors.confirmPassword && (
+              <p className="text-red-600 text-sm mt-1">
+                {errors.confirmPassword.message}
+              </p>
+            )}
           </div>
+
           <Button className="w-full text-md bg-[#2c4e9d] hover:bg-[#1d3470] text-white py-6 rounded-md">
             {SIGHN_UP_TEXT.signupButton}
           </Button>
-        </div>
+        </form>
+
         <p className="text-gray-600">
           {SIGHN_UP_TEXT.haveAccount}
-          <Link
-            to="/"
-            className="text-[#2c4e9d] font-semibold hover:underline cursor-pointer"
-          >
+          <Link to="/" className="text-[#2c4e9d] font-semibold hover:underline">
             {SIGHN_UP_TEXT.logIn}
           </Link>
         </p>
       </CardContent>
     </Card>
   );
-}
+};
+
+export default SignupForm;
